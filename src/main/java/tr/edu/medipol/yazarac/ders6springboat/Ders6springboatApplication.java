8bb511abb93ec26c0ff27c374c2bb6028c0ceb97
bprlab <bprlab@bprlab-iMac-61.local>
2019-03-30 14:01:13 +0300

    springboot

---
+++ src/main/java/tr/edu/medipol/yazarac/ders6springboat/Ders6springboatApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazarac.ders6springboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders6springboatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders6springboatApplication.class, args);
+	}
+
+}
